959e851cc708917ba9a82affc0aa847d998e740f
Dalington6 <d.tunyinko@alustudent.com>
2025-09-30 11:56:55 +0200

    Added users serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 
 from users.models import User, Passenger, Rider
-
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
+    """Serializer for user profile"""
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'phone_number', 'user_type']
-
+        fields = ('id', 'email', 'first_name', 'last_name', 'phone_number', 'user_type', 'date_joined')
+        read_only_fields = ('id', 'date_joined')
 
 class PassengerSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
@@ -34,3 +36,38 @@ class RiderSerializer(serializers.ModelSerializer):
             'average_rating', 'total_rides', 'total_earnings', 'created_at', 'updated_at'
         ]
         read_only_fields = ['id', 'average_rating', 'total_rides', 'total_earnings', 'created_at', 'updated_at']
+
+
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """Serializer for user registration"""
+    password = serializers.CharField(
+        write_only=True, 
+        required=True, 
+        validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'password2', 'first_name', 
+                  'last_name', 'phone_number', 'user_type')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
