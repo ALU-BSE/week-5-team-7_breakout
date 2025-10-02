778672188d2e6490e405b14d46ee146f4d770f98
Dalington6 <d.tunyinko@alustudent.com>
2025-10-02 12:12:05 +0200

    URL Configuration & Documentation

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
+from django.contrib import admin
 
-from users.views import UserViewSet, PassengerViewSet, RiderViewSet
+from users.views import UserViewSet, PassengerViewSet, RiderViewSet, RegisterView, LoginView, UserProfileView
+from rest_framework_simplejwt.views import TokenRefreshView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
@@ -9,4 +12,14 @@ router.register(r'passengers', PassengerViewSet, basename='passenger')
 router.register(r'riders', RiderViewSet, basename='rider')
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),    
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    
+    # API Documentation
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ] + router.urls
